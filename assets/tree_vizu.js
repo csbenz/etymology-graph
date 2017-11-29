cbda59418e9ce746b4a124bdc591a04bbef0e32c
Christopher Benz <christopher.benz@epfl.ch>
2017-11-29 01:05:25 +0100

    Update tree_vizu.js

--- assets/tree_vizu.js
+++ assets/tree_vizu.js
@@ -10,7 +10,7 @@ console.log(height);
 // append the svg object to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("body").select(".core_div").append("svg")
+var svg = d3.select(".core_div").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
